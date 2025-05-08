ad87a549640984a781c37ee7aa2d0b6ea5394ad2
aid098proton <aid098proton@gmail.com>
2025-05-09 03:34:51 +0900

    AI 노래 추가

--- pages/ai-music/ai-music.js
+++ pages/ai-music/ai-music.js
@@ -1,6 +1,14 @@
 // 음악 목록
 const musicList = [
     {
+        title: 'AI Music 20250509 1',
+        file: 'ai_music_20250509_1.mp3'
+    },
+    {
+        title: 'AI Music 20250509 2',
+        file: 'ai_music_20250509_2.mp3'
+    },
+    {
         title: 'AI Music 20250508 1',
         file: 'ai_music_20250508_1.mp3'
     },
