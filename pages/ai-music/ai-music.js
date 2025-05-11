d4f368109906fab2c8bcda9a756cf2dd8643e2f2
aid098proton <aid098proton@gmail.com>
2025-05-12 03:54:23 +0900

    AI 음악 추가

--- pages/ai-music/ai-music.js
+++ pages/ai-music/ai-music.js
@@ -1,6 +1,14 @@
 // 음악 목록
 const musicList = [
     {
+        title: 'AI Music 20250512 1',
+        file: 'ai_music_20250512_1.mp3'
+    },
+    {
+        title: 'AI Music 20250512 2',
+        file: 'ai_music_20250512_2.mp3'
+    },
+    {
         title: 'AI Music 20250511 1',
         file: 'ai_music_20250511_1.mp3'
     },
