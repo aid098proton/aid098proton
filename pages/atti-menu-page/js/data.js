a165d088abd735353d441f12a493acd40d1aaa8e
aid098proton <aid098proton@gmail.com>
2025-06-03 21:00:53 +0900

    아띠 메뉴 페이지 디자인 변경

--- pages/atti-menu-page/js/data.js
+++ pages/atti-menu-page/js/data.js
@@ -3,340 +3,135 @@ const data = {
         {
             category: "식빵 종류",
             items: [
-                {
-                    name: "쌀 식빵",
-                    price: 4300,
-                },
-                {
-                    name: "잡곡 식빵",
-                    price: 5500,
-                },
-                {
-                    name: "우유 식빵",
-                    price: 4600,
-                },
-                {
-                    name: "밤 식빵",
-                    price: 6800,
-                },
-                {
-                    name: "통호밀 빵",
-                    price: 6000,
-                },
-                {
-                    name: "통밀빵",
-                    price: 0,
-                }
+                { name: "쌀 식빵", price: 4300 },
+                { name: "잡곡 식빵", price: 5500 },
+                { name: "우유 식빵", price: 4600 },
+                { name: "밤 식빵", price: 6800 },
+                { name: "통호밀 빵", price: 6000 },
+                { name: "통밀빵", price: 0 }
             ]
         },
         {
             category: "빵 종류",
             items: [
-                {
-                    name: "쑥 쌀 찐빵 (3개입)",
-                    price: 3000,
-                },
-                {
-                    name: "팥빵 (2개입)",
-                    price: 3900,
-                },
-                {
-                    name: "당뇨에 좋은빵",
-                    price: 6000,
-                },
-                {
-                    name: "구리볼",
-                    price: 4000,
-                },
-                {
-                    name: "호두과자",
-                    price: 5000,
-                },
-                {
-                    name: "모닝빵",
-                    price: 2700,
-                },
-                {
-                    name: "크로와상",
-                    price: 2800,
-                },
-                {
-                    name: "맘 모스",
-                    price: 4200,
-                },
-                {
-                    name: "무화과빵",
-                    price: 6000,
-                },
-                {
-                    name: "치아바타",
-                    price: 4800,
-                },
-                {
-                    name: "소보로빵",
-                    price: 2000,
-                },
-                {
-                    name: "슈크림빵",
-                    price: 2500,
-                },
-                {
-                    name: "갈릭바게트",
-                    price: 5500,
-                },
-                {
-                    name: "소시지 피자",
-                    price: 4500,
-                },
-                {
-                    name: "베이컨롤 감자빵",
-                    price: 3800,
-                },
-                {
-                    name: "마늘 스틱",
-                    price: 1500,
-                },
-                {
-                    name: "바질 소세지 빵",
-                    price: 5500,
-                },
-                {
-                    name: "빵오레이즌",
-                    price: 4500,
-                },
-                {
-                    name: "피자빵",
-                    price: 4000,
-                },
-                {
-                    name: "소금빵 (플레인)",
-                    price: 2200,
-                },
-                {
-                    name: "소금빵 (먹물)",
-                    price: 2200,
-                },
-                {
-                    name: "꽈배기",
-                    price: 1500,
-                },
-                {
-                    name: "고로케",
-                    price: 4000,
-                },
-                {
-                    name: "화이트롤 빵",
-                    price: 4800,
-                },
-                {
-                    name: "생크림 팥빵",
-                    price: 3600,
-                },
+                { name: "쑥 쌀 찐빵 (3개입)", price: 3000 },
+                { name: "팥빵 (2개입)", price: 3900 },
+                { name: "당뇨에 좋은빵", price: 6000 },
+                { name: "구리볼", price: 4000 },
+                { name: "호두과자", price: 5000 },
+                { name: "모닝빵", price: 2700 },
+                { name: "크로와상", price: 2800 },
+                { name: "맘 모스", price: 4200 },
+                { name: "무화과빵", price: 6000 },
+                { name: "치아바타", price: 4800 },
+                { name: "소보로빵", price: 2000 },
+                { name: "슈크림빵", price: 2500 },
+                { name: "갈릭바게트", price: 5500 },
+                { name: "소시지 피자", price: 4500 },
+                { name: "베이컨롤 감자빵", price: 3800 },
+                { name: "마늘 스틱", price: 1500 },
+                { name: "바질 소시지 빵", price: 5500 },
+                { name: "빵오레이즌", price: 4500 },
+                { name: "피자빵", price: 4000 },
+                { name: "소금빵 (플레인)", price: 2200 },
+                { name: "소금빵 (먹물)", price: 2200 },
+                { name: "꽈배기", price: 1500 },
+                { name: "고로케", price: 4000 },
+                { name: "화이트롤 빵", price: 4800 },
+                { name: "생크림 팥빵", price: 3600 }
             ]
         },
         {
             category: "세트 상품",
             items: [
-                {
-                    name: "소금빵 3개",
-                    price: 6300,
-                },
-                {
-                    name: "소금빵 5개",
-                    price: 10000,
-                }
+                { name: "소금빵 3개", price: 6300 },
+                { name: "소금빵 5개", price: 10000 }
             ]
         },
         {
             category: "카스테라 종류",
             items: [
-                {
-                    name: "옥수수 카스테라",
-                    price: 6500,
-                }
+                { name: "옥수수 카스테라", price: 6500 }
             ]
         },
         {
             category: "베이글 종류",
             items: [
-                {
-                    name: "감자 베이글",
-                    price: 4500,
-                },
-                {
-                    name: "블루베리 베이글",
-                    price: 4500,
-                },
-                {
-                    name: "런던 베이글",
-                    price: 3600,
-                }
+                { name: "감자 베이글", price: 4500 },
+                { name: "블루베리 베이글", price: 4500 },
+                { name: "런던 베이글", price: 3600 }
             ]
         },
         {
             category: "케이크 종류",
             items: [
-                {
-                    name: "미니 케이크",
-                    price: 20000,
-                },
-                {
-                    name: "생크림 케이크 2호",
-                    price: 32000,
-                },
-                {
-                    name: "초코 롤 케이크",
-                    price: 20000,
-                },
-                {
-                    name: "화이트롤 케이크",
-                    price: 18000,
-                },
-                {
-                    name: "시몬 케이크",
-                    price: 5500,
-                },
-                {
-                    name: "초코 파운드 케이크",
-                    price: 4800,
-                },
-                {
-                    name: "몽블랑",
-                    price: 5300,
-                },
-                {
-                    name: "몽블랑 생크림",
-                    price: 6500,
-                }
+                { name: "미니 케이크", price: 20000 },
+                { name: "생크림 케이크 2호", price: 32000 },
+                { name: "초코 롤 케이크", price: 20000 },
+                { name: "화이트롤 케이크", price: 18000 },
+                { name: "시몬 케이크", price: 5500 },
+                { name: "초코 파운드 케이크", price: 4800 },
+                { name: "몽블랑", price: 5300 },
+                { name: "몽블랑 생크림", price: 6500 }
             ]
         },
         {
             category: "디저트/선물용",
             items: [
-                {
-                    name: "벌꿀 러스크",
-                    price: 5000,
-                },
-                {
-                    name: "피칸파이",
-                    price: 4500,
-                },
-                {
-                    name: "머랭",
-                    price: 3000,
-                },
-                {
-                    name: "블루베리 파이",
-                    price: 4800,
-                },
-                {
-                    name: "호두롱",
-                    price: 2200,
-                },
-                {
-                    name: "라이스 크렌노아",
-                    price: 0,
-                },
-                {
-                    name: "츄러스",
-                    price: 3800,
-                },
+                { name: "벌꿀 러스크", price: 5000 },
+                { name: "피칸파이", price: 4500 },
+                { name: "머랭", price: 3000 },
+                { name: "블루베리 파이", price: 4800 },
+                { name: "호두롱", price: 2200 },
+                { name: "라이스 크렌노아", price: 0 },
+                { name: "츄러스", price: 3800 }
             ]
         },
         {
             category: "팥빙수",
             special: true,
             items: [
-                {
-                    name: "아띠 팥빙수",
-                    price: 10000,
-                    packagingExtra: 6000
-                }
+                { name: "아띠 팥빙수", price: 10000, packagingExtra: 6000 }
             ]
         },
         {
             category: "음료",
             items: [
-                {
-                    name: "아이스커피",
-                    price: 3000,
-                },
-                {
-                    name: "매일우유 고소한맛",
-                    price: 0,
-                },
-                {
-                    name: "매일 우유 초코맛",
-                    price: 0,
-                },
-                {
-                    name: "매일 우유 딸기맛",
-                    price: 0,
-                },
-                {
-                    name: "피크닉",
-                    price: 0,
-                }
+                { name: "아이스커피", price: 3000 },
+                { name: "매일우유 고소한맛", price: 0 },
+                { name: "매일 우유 초코맛", price: 0 },
+                { name: "매일 우유 딸기맛", price: 0 },
+                { name: "피크닉", price: 0 }
             ]
         },
         {
             category: "떡",
             items: [
-                {
-                    name: "찹쌀떡",
-                    price: 0,
-                }
+                { name: "찹쌀떡", price: 0 }
             ]
         },
         {
             category: "햄버거, 샌드위치, 토스트",
             items: [
-                {
-                    name: "햄버거",
-                    price: 0,
-                },
-                {
-                    name: "샌드위치",
-                    price: 4900,
-                },
-                {
-                    name: "베이글 샌드위치",
-                    price: 5500,
-                },
-                {
-                    name: "콘치즈 토스트",
-                    price: 3000,
-                }
+                { name: "햄버거", price: 0 },
+                { name: "샌드위치", price: 4900 },
+                { name: "베이글 샌드위치", price: 5500 },
+                { name: "콘치즈 토스트", price: 3000 }
             ]
         },
         {
             category: "잼/소스",
             items: [
-                {
-                    name: "딸기쨈",
-                    price: 0,
-                }
+                { name: "딸기잼", price: 0 }
             ]
         },
         {
             category: "쿠키/스콘",
             items: [
-                {
-                    name: "초코칩 쿠키",
-                    price: 2200,
-                },
-                {
-                    name: "크랜베리 스콘",
-                    price: 2500,
-                },
-                {
-                    name: "후르츠링 쫀득쿠키",
-                    price: 0,
-                },
-                {
-                    name: "전병과자",
-                    price: 4500,
-                }
+                { name: "초코칩 쿠키", price: 2200 },
+                { name: "크랜베리 스콘", price: 2500 },
+                { name: "후르츠링 쫀득쿠키", price: 0 },
+                { name: "전병과자", price: 4500 }
             ]
         }
     ]
